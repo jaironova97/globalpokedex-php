0bc4c2a33590e5d4faeb3a6e53ba5d926a48e31a
jaironova97 <jairo.nova97@gmail.com>
2023-03-11 13:07:14 -0400

    update nosotros.php

--- nosotros.php
+++ nosotros.php
@@ -4,7 +4,7 @@
 
 <div class="jumbotron">
     <h1 class="display-3">Nosotros</h1>
-    <p class="lead">Hola, soy un juven empezando en el camino de la programacion web</p>
+    <p class="lead">Public pokedex</p>
     <hr class="my-2">
 </div>
 
